dff3f3d03de1489e5bc898d20416a3b349c4746d
Patrick Hum <patrick@ip-172-31-37-96.us-east-2.compute.internal>
2019-03-17 23:37:44 +0000

    Add helper run shell script run `. run.sh` file to run server on http://0.0.0.0:5000

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "If this doesn't work, run using . ./run.sh not ./run.sh";
+source backenv/bin/activate
+cd backend/python
+gunicorn -w 1 -b 0.0.0.0:5000 master_wsgi:app
+cd ../../
+deactivate
