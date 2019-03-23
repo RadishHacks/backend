83a6ec3a4e43b1e3541b5e74ff292e66e6130e24
Richard Cao <richardcao98@gmail.com>
2019-03-23 02:13:41 -0400

    added database access + write functions for api

--- session.py
+++ session.py
@@ -15,3 +15,12 @@ class Session(Document):
     last_active_timestamp = DateTimeField(required=True, default=datetime.datetime.utcnow)
     created_timestamp = DateTimeField(required=True, default=datetime.datetime.utcnow)
     image_pairs_voted = ListField(ObjectIdField())
+
+    def create_session(self):
+        return Session().save()
+
+    def update_active_session(self, session_id):
+        Session.objects(id=session_id).update_one(set__last_active_timestamp=datetime.datetime.utcnow())
+
+    def update_voted_history(self, session_id, image_pair_id):
+        Session.objects(id=session_id).update_one(push__image_pairs_voted=image_pair_id)
