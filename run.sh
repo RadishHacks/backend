ab4fdbc5a139109c2bbe3087841417d6bb7fe283
Ubuntu <ubuntu@ip-172-31-25-135.us-east-2.compute.internal>
2019-06-12 07:40:45 +0000

    put gunicorn in background for run script

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 echo "If this doesn't work, run using . ./run.sh not ./run.sh";
 source backenv/bin/activate
 cd backend/python
-gunicorn -w 1 -b 0.0.0.0:5000 master_wsgi:app
+gunicorn -w 1 -b 0.0.0.0:5000 master_wsgi:app --daemon
 cd ../../
 deactivate
